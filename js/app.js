065674201a94db3af0c21cb21a95aaf492bc5db9
IrinaKuz <ira.kuznets@gmail.com>
2020-07-15 17:03:51 -0400

    Fixed bug with computer board rendering and cleaned code

--- js/app.js
+++ js/app.js
@@ -10,16 +10,31 @@ comp_gameboard.chooseRandomShips();
 let user = new Player('user', my_gameboard, new Gameboard());
 let computer = new Player('computer', comp_gameboard, new Gameboard());
 
-console.log(my_gameboard);
-console.log(comp_gameboard);
-
 async function drawBoards() {
-  await user.my_gameboard.drawBoard(my_board);
   await user.op_gameboard.drawBoard(comp_board);
-  await user.my_gameboard.renderBoard(my_board);
   cells_comp = comp_board.querySelectorAll('.cell');
 }
 
+function loop(coord) {
+      let result = computer.my_gameboard.receiveAttack(coord);
+      user.op_gameboard.updateBoard(coord, result);
+      if(result == 'lost') {
+        user.op_gameboard.endGame('You won!');
+      }
+
+      // redraw computer's board
+      user.op_gameboard.renderBoard(comp_board);
+
+      let comp_coord = computer.makeMove();
+      result = user.my_gameboard.receiveAttack(comp_coord);
+      user.op_gameboard.renderBoard(comp_board);
+      if(result == 'lost') {
+        user.op_gameboard.endGame('You lost!');
+      }
+
+      // redraw user's board
+      user.my_gameboard.renderBoard(my_board);
+}
 
 function getCoords(e) {
   const cell = this;
@@ -32,7 +47,7 @@ function getCoords(e) {
 }
 
 async function addCellListeners() {
-  //await drawBoards();
+  await drawBoards();
   for (let i = 0; i < cells_comp.length; i++) {
     cells_comp[i].addEventListener('click', getCoords);
   }
@@ -91,7 +106,8 @@ function onChooseShip(e) {
   }
 }
 
-drawBoards();
+user.my_gameboard.drawBoard(my_board);
+user.my_gameboard.renderBoard(my_board);
 my_board.addEventListener('dblclick', onChooseShip);
 
 // Start the game when user clicks on 'start game' button
@@ -101,27 +117,3 @@ start_game.addEventListener('click', function() {
     my_board.removeEventListener('dblclick', onChooseShip);
     addCellListeners();
 });
-
-function loop(coord) {
-      let result = computer.my_gameboard.receiveAttack(coord);
-      user.op_gameboard.updateBoard(coord, result);
-      if(result == 'lost') {
-        user.op_gameboard.endGame('You won!');
-      }
-
-      // redraw computer board
-      user.op_gameboard.renderBoard(comp_board);
-
-      let comp_coord = computer.makeMove();
-      result = user.my_gameboard.receiveAttack(comp_coord);
-      computer.op_gameboard.renderBoard(comp_board);
-      if(result == 'lost') {
-        user.op_gameboard.endGame('You lost!');
-      }
-
-      // redraw my_board
-      user.my_gameboard.renderBoard(my_board);
-
-      console.log(user.my_gameboard);
-      console.log(computer.my_gameboard);
-}
